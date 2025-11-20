35a769dc6b69f0e83eb23f74a93cb8acc82717f7
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2025-11-20 10:13:21 +0100

    feat: add tactic subst_last

--- Trale/Utils/Normalize.lean
+++ Trale/Utils/Normalize.lean
@@ -39,3 +39,51 @@ macro "tr_flip" : tactic => `(tactic|
 macro "tr_root_R" : tactic => `(tactic|
   first |apply Param.flip |apply flipR |apply flipR'
   )
+
+
+syntax (name := subst_last) "subst_last" : tactic
+
+-- BEGIN Source: Based on `subst` tactic (evalSubst)
+-- Modified
+open Lean Elab Tactic Meta Term
+
+elab_rules : tactic
+  | `(tactic| subst_last%$t) =>
+    withMainContext do
+      let lctx ← getLCtx
+      let mvarId ← getMainGoal
+
+      let hs := lctx.getFVarIds
+      let hsSize := hs.size
+
+      let fvarId ←
+        if h: hsSize > 0 then
+          pure hs[hsSize - 1]
+        else
+          throwTacticEx `subst_last mvarId "No last hypothesis"
+
+      -- let fvarId ← match hs.toList with
+      --   | [] => throwTacticEx `subst_last mvarId "No last hypothesis"
+      --   | a::_ => pure a
+
+      trace[debug] s!"Last hypothesis: {←fvarId.getUserName}"
+
+      -- forEachVar (hs) fun mvarId fvarId => do
+      let newMVarId ← do
+        let decl ← fvarId.getDecl
+        if decl.isLet then
+          -- Zeta delta reduce the let and eliminate it.
+          let (_, mvarId) ← mvarId.withReverted #[fvarId] fun mvarId' fvars => mvarId'.withContext do
+            let tgt ← mvarId'.getType
+            assert! tgt.isLet
+            let mvarId'' ← mvarId'.replaceTargetDefEq (tgt.letBody!.instantiate1 tgt.letValue!)
+            -- Dropped the let fvar
+            let aliasing := (fvars.extract 1).map some
+            return ((), aliasing, mvarId'')
+          pure mvarId
+        else
+          Meta.subst mvarId fvarId
+
+      replaceMainGoal [newMVarId]
+  -- | _                     => throwUnsupportedSyntax
+-- END Source

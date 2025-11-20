a0e70d779e5b44bf7ae97f70e287d761927ed82e
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2025-11-20 10:19:30 +0100

    feat: add experimental tr_advance

---
+++ TraleTest/Utils/Lemmas/TrAdvance.lean
@@ -0,0 +1,125 @@
+import Mathlib
+
+import Trale.Core.Param
+import Trale.Utils.Extend
+import Trale.Utils.Split
+import Trale.Utils.Simp
+import Trale.Utils.ParamIdent
+import Trale.Utils.Normalize
+
+import Trale.Theories.Forall
+import Trale.Theories.Sorts
+
+import TraleTest.Utils.Lemmas.Modulo
+
+open Trale.Utils
+
+def refoldMap10
+  : Param10 α β → Param MapType.Map1 MapType.Map0 α β := id
+
+theorem refoldMap10Eq
+  : Param10.{w} α β = Param.{w} MapType.Map1 MapType.Map0 α β := rfl
+
+-- theorem paramToEq {a b : γ}
+--   (x : Param_ident.instParam44 γ) : α = β := by
+--     exact x.R
+
+
+
+macro "tr_step_rel" : tactic => do
+  let o1 ← `(tactic|apply flipR')
+  let o2 ← `(tactic|rw [←Param_ident.param44_ident_symm])
+  --
+  let main ← `(tactic|
+    (apply denormalizeR;
+    --  (first| apply arrow_02a_rel);
+     apply_assumption;
+     run_tac
+      trace[tr.utils] "Did arrow splitting"
+     assumption
+    )
+  )
+  --
+  `(tactic|
+    first
+    | $main;
+    | $o2; $main;
+    | $o1; $main;
+    | $o1; $o2; $main;
+  )
+
+set_option trace.tr.utils true
+
+-- example : Param40 Nat String := by
+--   show Param .Map4 .Map0 Nat String
+
+--   sorry
+
+
+
+-- macro "tr_refold" : tactic => `(tactic|
+--   focus (
+--     show Param _ _ _ _
+
+--   )
+--   )
+
+macro "tr_advance" : tactic => `(tactic|
+  (
+  --   (run_tac
+  --   -- This will produce the 'No goals to be solved' error if we are done.
+  --   Lean.Elab.Tactic.getMainGoal
+  -- );
+  focus
+    first
+    | assumption
+    | infer_instance
+    | (first
+      | tr_intro _ _ _
+      | tr_flip; tr_intro _ _ _
+      | tr_split
+      | tr_flip; tr_split); try subst_last
+    | exact (Param44_ident'' rfl).forget
+    | (intro x x' xR; subst xR)
+    | exact congrArg _
+    | (
+        let : Param.R _ _ _ _ := by assumption;
+        rw [this]
+      )
+    | apply_assumption
+    | (
+        tr_split_application';
+        -- case' p2 => intro _ _ _
+        first
+        | case p1 => infer_instance
+          case' aR => tr_whnf
+        | case' aR => tr_whnf
+          case' p1 => skip -- Fix the ordering
+
+        -- case' p1 => try infer_instance
+        -- case' aR => tr_whnf
+        -- case' p1 => skip -- Fix the ordering. But this causes error if p1 is
+                            -- already closed.
+        )
+    | (
+        tr_flip;
+        tr_split_application';
+        -- case' p2 => intro _ _ _
+        first
+        | case p1 => infer_instance
+          case' aR => tr_whnf -- The tr_whnf will a.o. remove the inferInstance
+        | case' aR => tr_whnf
+          case' p1 => skip -- Fix the ordering
+      )
+
+    | (refine (Param_ident.instantiatePropR_bi ?_).forget;
+       tr_step_rel)
+    | (refine (instantiatePropR ?_).forget; tr_step_rel)
+    | fail "No step available"
+    )
+  )
+
+#check Param_ident.instantiatePropR_bi
+#check instantiatePropR
+#check flipR'
+#check Param_ident.param44_ident_symm
